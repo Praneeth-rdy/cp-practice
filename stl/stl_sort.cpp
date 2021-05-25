eef4b62dbb174c4953b00d7c611f17eff4be49d5
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2021-05-25 09:36:00 +0530

    Added stl sorting algo in the header file <algorithm>

---
+++ stl/stl_sort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+void show_vector(vector<int> myvector){
+    vector<int>::iterator itr = myvector.begin();
+
+    while(itr != myvector.end()){
+        cout << *itr << " ";
+        itr++;
+    }
+    cout << "\n";
+}
+// ** comparison function decides whether the passed "first" argument should be placed before the passed "second" argument or not.
+// Comparison function to sort in descending order
+// Checks whether b should be placed after a in the array
+bool comp(int &a, int &b){
+    // implies that b should be placed after a, iff b is less than a
+    return a > b;
+}
+
+int main(){
+    vector<int> myvector = {1, 5, 3, 2, 6, 4};
+    sort(myvector.begin(), myvector.end(), comp);
+    show_vector(myvector);
+    // returns true if the range is sorted in the ascending order
+    // You can also pass a similar comp function to is_sorted function
+    cout << is_sorted(myvector.begin(), myvector.end()) << "\n";
+    return 0;
+}
+
+/*
+** Sort function is not stable
+** Time complexity to sort is O(nlogn)
+A set of requirements expected by some of the standard libraries from the user-provided functions is called Compare.
+So the function we pass to sort as argument should satisfy the requirements of compare
+1) Return type should be convertible to bool
+2) for all a, function(a, a) == false
+3) if function(a, b) == true, function(b, a) == false
+4) if function(a, b) and function(b, c) are true, then function(a, c) == true
+*/
